278963ee96326a6b6c3bf2d61a171b2904499bc2
Amit Kumar Ranjan <iamakr97@gmail.com>
2024-12-03 12:03:22 +0530

    updated routes for super admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const { auth } = require('../middlewares/auth');
 const { sendSignupOTP, signupUser } = require('../controllers/userAuth/registerUser');
 const { login } = require('../controllers/userAuth/loginUser');
