ecebb50e24d8959e39b1d89db83ff71d4baf0d6a
Manav Jain <nit474011gwl@gmail.com>
2025-02-12 00:52:37 +0530

    responsive header, course skeleton, add gradient div on homepage

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,107 +1,86 @@
 import logo from "../../../public/logo.svg";
-import bar from "../../../public/imgs/slider/Hamburger_icon.png";
+import { CiSearch } from "react-icons/ci";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+
 import { logout } from "../../store/reducers/authReducer";
+import HamNavbar from "./HamNavbar";
 
 export default function Header() {
   const { token } = useSelector((store) => store.auth);
 
   const dispatch = useDispatch();
 
-  // useState
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   const handleLogout = async () => {
     dispatch(logout());
   };
-  return (
-    // Header
-    <nav className="bg-white border-gray-200 w-full">
-      <div
-        className={`max-w-screen-xl flex flex-wrap items-center gap-20 mx-auto p-4 justify-around md:ml-[10%] sm:justify-normal`}
-      >
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
 
-        <div className=" md:order-2">
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="sm:hidden text-gray-500 rounded-lg text-sm p-2.5 me-1 "
-            aria-controls="navbar-search"
-            aria-expanded={isMenuOpen}
-          >
-            <img src={bar} alt="bar" className="w-[30px] h-[30px]" />
-            <span className="sr-only">Search</span>
-          </button>
+  return (
+    <nav className="flex items-center gap-24 h-20 py-4 px-10 sm:px-20 md:px-32">
+      {/* Logo */}
+      <Link to="/">
+        <img
+          src={logo}
+          alt="logo"
+          className="min-w-[200px] w-[200px] h-[45px]"
+        />
+      </Link>
+
+      {/* For Bigger Screen */}
+      <div className="hidden font-bold md:flex space-x-24">
+        {/* Second Section */}
+        <div className="relative">
+          <CiSearch className="absolute left-2 top-3 size-6 text-[var(--color-primary)]" />
+          <input
+            type="text"
+            placeholder="Type to search"
+            className="font-normal text-lg pl-10 px-2 py-2 border rounded-md outline-none"
+          />
         </div>
 
-        <div
-          className={`items-center justify-between ${
-            isMenuOpen ? "block" : "hidden"
-          } w-full md:flex md:w-auto md:order-1 `}
-          id="navbar-search"
-        >
-          <div className=" relative w-auto md:w-1/3 mt-[-60px] md:mt-0 rounded-[14px] lg:w-[300px]">
-            <span className="absolute inset-y-0 left-3 flex items-center">
-              <MagnifyingGlassIcon className="h-5 w-5 text-[#0b66c3]" />
-            </span>
-
-            <input
-              type="text"
-              placeholder="Type to search"
-              className="w-auto pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-[#f1f2f5] text-[#707070] font-sans"
-            />
-          </div>
-
-          <Link
-            to="/course"
-            className="text-[var(--color-primary)] font-sans mt-4  md:mt-0  md:ml-10"
-          >
+        {/* Third Section */}
+        <div className="flex items-center gap-10">
+          <Link to="/course" className="text-[var(--color-primary)]">
             Learning
           </Link>
 
           {/* <Link
-            to=""
-            className="text-[var(--color-primary)] font-sans mt-4  md:mt-0  md:ml-10"
-          >
-            Resource
-          </Link> */}
+              to=""
+              className="text-[var(--color-primary)]"
+            >
+              Resource
+            </Link> */}
 
-          <Link
-            to="/blog"
-            className="text-[var(--color-primary)] font-sans mt-4  md:mt-0  md:ml-10"
-          >
+          <Link to="/blog" className="text-[var(--color-primary)]">
             Blog
           </Link>
 
           {/* <Link
-            to=""
-            className="text-[var(--color-primary)] font-sans mt-4  md:mt-0  md:ml-10"
-          >
-            Contact Us
-          </Link> */}
+              to=""
+              className="text-[var(--color-primary)]"
+            >
+              Contact Us
+            </Link> */}
 
           {token ? (
             <button
               onClick={handleLogout}
-              className="text-[var(--color-primary)] font-sans mt-4 md:mt-0 md:ml-10 cursor-pointer"
+              className="text-[var(--color-primary)]"
             >
               Logout
             </button>
           ) : (
-            <Link
-              to="/auth"
-              className="text-[var(--color-primary)] font-sans mt-4 md:mt-0 md:ml-10"
-            >
+            <Link to="/auth" className="text-[var(--color-primary)]">
               Login
             </Link>
           )}
         </div>
+      </div>
+
+      {/* For Small Screen */}
+      <div className="font-bold md:hidden ml-auto">
+        <HamNavbar handleLogout={handleLogout} />
       </div>
     </nav>
   );
