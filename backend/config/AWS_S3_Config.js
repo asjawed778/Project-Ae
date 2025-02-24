23864a2bf290618112779410d4ccdaabb3e4cc5d
Abhishek Kamati <105870308+AbhiK2244@users.noreply.github.com>
2025-02-25 00:46:00 +0530

    Delete backend directory

--- backend/config/AWS_S3_Config.js
+++
@@ -1,9 +0,0 @@
-const { S3Client } = require("@aws-sdk/client-s3");
-
-exports.s3Client = new S3Client({
-    region: process.env.S3_REGION_NAME,
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    }
-})
