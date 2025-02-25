4f156c51e9574a385f0573174c8ce45f9ab884f0
Abhishek Kamati <kamtiabhi4@gmail.com>
2025-02-25 15:16:56 +0530

    Merged server folder from development branch

--- server/app/course/course.route.ts
+++ server/app/course/course.route.ts
@@ -2,11 +2,18 @@
 import { Router } from "express";
 import { catchError } from "../common/middleware/cath-error.middleware";
 import * as authMiddlerware from "../common/middleware/auth.middleware";
-import * as courseController from "../category/category.controller";
+import * as courseController from "./course.controller";
 import * as courseValidation from "./course.validation";
+import * as fileUploadMiddleware from "../common/middleware/fileUpload.middleware";
 const router = Router();
 
 router
-    .post('/category', authMiddlerware.auth, courseValidation.createCourseCategory, catchError, )
-        
+    .post("/thumbnail", fileUploadMiddleware.thumbnailUpload, catchError, courseController.uploadfile)
+    .post("/brouchure", fileUploadMiddleware.brouchureUpload, catchError, courseController.uploadfile)
+    .post("/details", courseValidation.courseDetails, catchError, courseController.addCourseDetails)
+    .put("/details/:courseId", courseValidation.courseDetails, catchError, courseController.addCourseDetails)
+    .post("/trailer-video", fileUploadMiddleware.videoUpload, catchError, courseController.uploadfile)
+    .put("/additional-details/:courseId", courseValidation.AdditionalDetails, catchError, courseController.addAdditionalDetails)
+    .get("/instructors", courseController.getIntructorList)
+
 export default router;
