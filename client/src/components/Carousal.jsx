422df63270b812fca9464917b188e81b0db6675f
Abhishek Kamati <kamtiabhi4@gmail.com>
2025-03-01 21:25:04 +0530

    bugs fixes

--- client/src/components/Carousal.jsx
+++ client/src/components/Carousal.jsx
@@ -101,7 +101,7 @@ export default function Carousal() {
 
       {/* Scrollable Course Cards */}
       {!(allCategoriesLoader || categoriesCourseLoader) ? (
-        <div className="flex items-center flex-wrap md:flex-nowrap gap-5 p-4 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10 p-4 mx-auto md:mx-0">
           {coursesAll?.map((course, index) => (
             <Link
               key={index}
