f35977d8f85fb6c7d56dcce93b7e1edaf181873f
Asif Jawed <asjawed778@gmail.com>
2025-03-07 01:23:01 +0530

    Delete server directory

--- server/app/category/category.dto.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { BaseSchema } from "../common/dto/base.dto";
-
-export interface ICourseCategory extends BaseSchema {
-    name: string;
-    description: string;
-    courses?: mongoose.Types.ObjectId[];
-}
