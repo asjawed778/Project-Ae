2f6a53f8fcd3ded6eee2b110825b86e660a34c0a
Abhishek Kamati <kamtiabhi4@gmail.com>
2025-03-07 13:01:36 +0530

    slider added in homepage for courses

--- frontend/src/components/Carousal.jsx
+++ frontend/src/components/Carousal.jsx
@@ -1,12 +1,13 @@
 import user from "../../public/imgs/slider/user_icon2.png";
 import clock from "../../public/imgs/slider/language2.png";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setCourses } from "../store/reducers/coursesReducer";
 import { setCategories } from "../store/reducers/adminCategoryReducer";
 import CourseSkeleton from "./skeletons/CourseSkeleton";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import {
   useGetAllCategoryQuery,
   useGetCategoryCourseQuery,
@@ -23,15 +24,15 @@ export default function Carousal() {
 
   const categories = useSelector((state) => state.categories.categories);
   const coursesAll = useSelector((state) => state.courses.courses);
-  
+
   const [activeTab, setActiveTab] = useState(null);
-  
+
   const { data: allCategories, isLoading: allCategoriesLoader } =
-  useGetAllCategoryQuery();
+    useGetAllCategoryQuery();
   const { data: categoryCourse, isLoading: categoriesCourseLoader } =
-  useGetCategoryCourseQuery(activeTab, {
-    skip: !activeTab,
-  });
+    useGetCategoryCourseQuery(activeTab, {
+      skip: !activeTab,
+    });
 
   useEffect(() => {
     if (allCategories?.success) {
@@ -51,8 +52,24 @@ export default function Carousal() {
     }
   }, [activeTab, categoryCourse, dispatch]);
 
+  const sliderRef = useRef(null);
+
+  // Function to scroll left
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({ left: -328, behavior: "smooth" });
+    }
+  };
+
+  // Function to scroll right
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({ left: 328, behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="p-8 mt-4 w-full mx-auto">
+    <div className="p-8 mt-4 w-[82vw] lg:w-full mx-auto">
       <div className="mb-6 items-center">
         <h2 className="font-sans font-semibold text-2xl text-[var(--color-primary)]">
           All the skills you need in one place
@@ -87,12 +104,12 @@ export default function Carousal() {
           )}
       </div>
 
-      <div className="relative w-[80%]">
+      <div className="relative md:w-[90%] w-full">
         <hr className="border-gray-200" />
         {coursesAll?.length !== 0 && (
           <Link
             to="/course"
-            className="absolute right-0 -top-3 font-bold text-xs text-[var(--color-primary)] bg-white px-5"
+            className="absolute right-0 -top-3 font-bold text-xs text-[var(--color-primary)] bg-white mx-auto"
           >
             View More
           </Link>
@@ -101,56 +118,84 @@ export default function Carousal() {
 
       {/* Scrollable Course Cards */}
       {!(allCategoriesLoader || categoriesCourseLoader) ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10 p-4 mx-auto md:mx-0">
-          {coursesAll?.map((course, index) => (
-            <Link
-              key={index}
-              to={`/course/${course?._id}`}
-              className="bg-white flex flex-col gap-2 w-[296px] pb-3 rounded-lg shadow-md mx-auto md:mx-0"
+        <div className="relative w-full">
+          {/* Left Button */}
+          {coursesAll?.length > 1 && (
+            <button
+              onClick={scrollLeft}
+              className="absolute h-8 w-8 flex justify-center items-center -left-10 top-1/2 transform -translate-y-1/2 z-10  bg-primary hover:bg-primary-hover cursor-pointer text-white text-2xl rounded-full shadow-md mx-auto"
             >
-              <img
-                src={course?.thumbnail}
-                alt={course?.title}
-                className="w-[100%] h-40 rounded-lg object-cover"
-              />
-              <div className="flex flex-col px-5">
-                <h3 className="font-sans text-[var(--color-secondary)]">
-                  {course?.title}
-                </h3>
+              <IoIosArrowBack />
+            </button>
+          )}
 
-                <hr className="mt-5 border-gray-200" />
-                <div className="text-xs flex flex-row items-center justify-between gap-2 h-[38px]">
-                  <div className="flex flex-row items-center gap-1">
-                    <img
-                      src={user}
-                      alt="userLive"
-                      className="w-[25px] h-[25px]"
-                    />
-                    <p className="text-gray-600">
-                      {course?.courseMode?.charAt(0).toUpperCase() +
-                        course?.courseMode?.slice(1).toLowerCase()}
-                    </p>
-                  </div>
-
-                  <div className="flex flex-row items-center gap-1">
-                    <img
-                      src={clock}
-                      alt="clock"
-                      className="w-[25px] h-[25px]"
-                    />
-                    <p className="text-gray-600">
-                      {course?.language?.charAt(0).toUpperCase() +
-                        course?.language?.slice(1).toLowerCase()}
-                    </p>
-                  </div>
-
-                  <Button className="text-white whitespace-nowrap px-3 py-1 ml-auto rounded-md duration-200">
-                    Learn More
-                  </Button>
+          {/* Scrollable Container */}
+          <div
+            ref={sliderRef}
+            className="flex overflow-x-scroll scroll-smooth scrollbar-hide gap-8 md:gap-7 p-4 whitespace-nowrap custom-scrollbar"
+          >
+            {coursesAll?.map((course, index) => (
+              <Link
+                key={index}
+                to={`/course/${course?._id}`}
+                className="bg-white flex flex-col h-70 gap-2 w-[300px] pb-3 rounded-lg shadow-md flex-none"
+              >
+                <div className="w-full h-44 relative rounded-lg">
+                  <img
+                    src={course?.thumbnail}
+                    alt={course?.title}
+                    className="w-full h-full absolute rounded-lg object-cover"
+                  />
                 </div>
-              </div>
-            </Link>
-          ))}
+                <div className="flex flex-col px-3 w-full">
+                  <h3 className="font-sans text-[var(--color-secondary)] w-full overflow-hidden text-ellipsis whitespace-nowrap">
+                    {course?.title}
+                  </h3>
+
+                  <hr className="mt-4 border-gray-200 mb-5 flex-grow" />
+                  <div className="text-xs flex items-center justify-between gap-2 h-[38px]">
+                    <div className="flex flex-row items-center gap-1">
+                      <img
+                        src={user}
+                        alt="userLive"
+                        className="w-[25px] h-[25px]"
+                      />
+                      <p className="text-gray-600">
+                        {course?.courseMode?.charAt(0).toUpperCase() +
+                          course?.courseMode?.slice(1).toLowerCase()}
+                      </p>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-1">
+                      <img
+                        src={clock}
+                        alt="clock"
+                        className="w-[25px] h-[25px]"
+                      />
+                      <p className="text-gray-600">
+                        {course?.language?.charAt(0).toUpperCase() +
+                          course?.language?.slice(1).toLowerCase()}
+                      </p>
+                    </div>
+
+                    <Button className="text-white whitespace-nowrap px-3 py-1 ml-auto rounded-md duration-200">
+                      Learn More
+                    </Button>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+
+          {/* Right Button */}
+          {coursesAll?.length > 1 && (
+            <button
+              onClick={scrollRight}
+              className="absolute h-8 w-8 flex justify-center items-center -right-10 top-1/2 transform -translate-y-1/2 z-10 bg-primary hover:bg-primary-hover cursor-pointer text-white text-2xl rounded-full shadow-md"
+            >
+              <IoIosArrowForward />
+            </button>
+          )}
         </div>
       ) : (
         <CourseSkeleton />
